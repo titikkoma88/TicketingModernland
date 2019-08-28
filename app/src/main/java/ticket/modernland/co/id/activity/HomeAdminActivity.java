a7c1252a0925c9e299dde6ccb07db0712026ef4d
titikkoma88 <jie.koma88@gmail.com>
2019-08-28 09:59:07 +0700

    Menambahkan HomeAdminActivity

---
+++ app/src/main/java/ticket/modernland/co/id/activity/HomeAdminActivity.java
@@ -0,0 +1,15 @@
+package ticket.modernland.co.id.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ticket.modernland.co.id.R;
+
+public class HomeAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_admin);
+    }
+}
